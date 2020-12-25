82e695d6f3851d946b1de508be1d99fb0837c87e
Kwikswap <simon@organikmedia.com.au>
2020-12-25 17:30:10 +0500

    changings

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,7 +1,7 @@
 import { INIT_CODE_HASH } from '../src/constants'
 
 // this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
-// and load the JSON 
+// and load the JSON
 const COMPUTED_INIT_CODE_HASH = '0xbc919ae6f6f95dca1e223fc957286afa1da81529418e9f187db8a0b2d2e963bc'
 
 describe('constants', () => {
